c24531113841db84d32260c0a6b14721fb28b6bd
Nathan <nazzanuk@gmail.com>
2017-10-16 10:04:18 +0100

    :)

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ const app = express(),
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use(express.static('./../release'));
+// app.use(express.static('./../release'));
 app.set('view engine', 'ejs');
 
 const server = app.listen(port, () => {
